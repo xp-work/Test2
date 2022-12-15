42abcd11f726761b8a16690f6c694e0d54da98bf
AlbertXiaoPeng <Albert.xiao@grapecity.com>
2022-12-15 15:24:03 +0900

    chore(other): fix config

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-var _AUTHORITY_ENDPOINT = 'https://nsp.authing.cn/oidc';
-var _API_END_POINT = 'https://mock.apifox.cn/m1/1149151-0-default';
-var _ICON_FONT_URL = '//at.alicdn.com/t/c/font_3501696_lfwf3jpe0vm.js';
+var _AUTHORITY_ENDPOINT = "https://nsp.authing.cn/oidc";
+var _API_END_POINT = "https://mock.apifox.cn/m1/1149151-0-default";
+var _ICON_FONT_URL = "//at.alicdn.com/t/c/font_3501696_lfwf3jpe0vm.js";
