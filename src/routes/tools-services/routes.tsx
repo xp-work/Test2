d73237c6dde660f2caf512321e08fd3ee785ed85
AlbertXiaoPeng <Albert.xiao@grapecity.com>
2022-12-16 15:45:29 +0900

    fix(guid): name to uuid

--- src/routes/tools-services/routes.tsx
+++ src/routes/tools-services/routes.tsx
@@ -2,11 +2,11 @@ import { AppRouteObject } from "../app-routes";
 import HmacRouter from "./hmac/router";
 import OneWayEncryptionRouter from "./one-way-encryption/router";
 import RandomStringRouter from "./random-string/router";
-import GuidPageRouter from "./guid-page/router";
+import UuidPageRouter from "./uuid-page/router";
 
 export const ToolsServices: AppRouteObject[] = [
     HmacRouter,
     OneWayEncryptionRouter,
     RandomStringRouter,
-    GuidPageRouter
+    UuidPageRouter
 ];
