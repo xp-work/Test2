608c54b6fdb17ea11a4ac3eb4bbf1c242cb4008c
SpiritLing <spiritling@outlook.com>
2023-07-21 15:55:17 +0900

    fix(netlify): add netlify cmd and config

--- apps/tools-site/public/config.js
+++ apps/tools-site/public/config.js
@@ -1,8 +1,8 @@
 var _ICON_FONT_URL_ = "//at.alicdn.com/t/c/font_3501696_fewr4ttysjg.js";
 var _ICON_PARK_URL_ =
 	"//lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_15525_44.1756bac6b6407846e6cfc404955d05b2.js";
-var _BAIDU_COUNT_ = "";
-var _GOOGLE_TAG_ = "";
+var _BAIDU_COUNT_ = "b97145d26117b0c237e9ca2c40ff24d2";
+var _GOOGLE_TAG_ = "G-B1MDEHPYWL";
 var _AUTHORITY_ENDPOINT_ = "https://nsp.authing.cn/oidc";
 var _APIFOX_ENDPOINT_ = "https://mock.apifox.cn/m1/2738694-0-default";
 var _APIFOX_TOKEN_ = "i0XkJ5G8xX2R6LmG4v8sjsLuWQt2NUnf";
